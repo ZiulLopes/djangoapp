86ec9b0e3102dee346bd8ffa4f0101ebe463d2fa
Luiz Fernando Lopes <lopesluiz_@hotmail.com>
2018-12-18 20:32:29 -0200

    working include

--- myappdjango/accounts/views.py
+++ myappdjango/accounts/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
 
-    programing_languages = ['python', '.net', 'android', 'swift', 'asp', 'javascript']
+    programing_languages = ['python', '.net', 'android', 'swift', 'asp', 'javascript', 'java']
     name = 'Luiz Fernando lopes'
     args = {'name': name, 'programing_languages': programing_languages}
 
     return render(request, "accounts/home.html", args)
 
 def login(request):
-    return render(request, "accounts/login.html");
+    return render(request, "accounts/login.html")
